e73ed11155301dfd9d3df7ea3b7052a38417ae6c
ricardo <rcavaraujo@gmail.com>
2022-07-24 17:00:16 -0300

    [M1S04] Ex 4 - Conta bancaria Parte 4

--- index.js
+++ index.js
@@ -16,15 +16,59 @@ const contasClientes = [
     },
     ];
 
-    const adicionaContas = () => {
-        const idContas = document.getElementById("contas");
-        contasClientes.forEach(conta => {
-            idContas.innerHTML += `<option>${conta.nome} ${conta.saldo}</option>`
+const adicionaContas = () => {
+    const idContas = document.getElementById("contas");
+    contasClientes.forEach(conta => {
+        idContas.innerHTML += `<option id="${conta.id}">${conta.nome} ${conta.saldo}</option>`
 
-            /* const option = document.createElement("option");
-            option.textContent = `${conta.nome} ${conta.saldo}`;
-            idContas.appendChild(option); */
-        })
+        /* const option = document.createElement("option");
+        option.textContent = `${conta.nome} ${conta.saldo}`;
+        idContas.appendChild(option); */
+    })
+}
+
+
+const executa = document.getElementById("executa");
+executa.onclick = () => {
+    const optionOperacao = document.getElementById("operacao");
+    const selecaoOperacao = optionOperacao.options[optionOperacao.selectedIndex].text;
+    const valor = document.getElementById("valor").value;
+    const optionConta = document.getElementById("contas");
+    let selecaoConta = optionConta.options[optionConta.selectedIndex].id;
+    selecaoConta = parseInt(selecaoConta);
+    //const selecaoContaInt = parseInt(selecaoConta)
+    const conta = contasClientes.find(conta => conta.id === selecaoConta)
+    console.log(selecaoConta)
+    console.log(conta)
+    console.log(selecaoOperacao)
+    if(selecaoOperacao.toLowerCase() === "sacar") {
+        console.log('chegou aqui')
+        saca(conta, valor)
+    } else if(selecaoOperacao === "depositar") {
+        deposita (conta, valor)
     }
+    
+}
 
-    document.body.onload = adicionaContas();
+// executa.addEventListener('click', saca);
+
+const saca = (conta, valor) => {
+    if(valor <= 0) {
+        const mensagem = document.getElementById("mensagem");
+        mensagem.textContent = "Valor menor do que 0"
+    } else if(valor > conta.saldo) {
+        mensagem.textContent = `Saldo insuficiente. Saldo: ${conta.saldo}`
+    } else {
+        conta.saldo = conta.saldo - valor;
+        mensagem.textContent = `Saque realizado. Saldo: ${conta.saldo}`
+        console.log(contasClientes)
+    }
+    
+}
+
+const deposita = (conta, valor) => {
+
+}
+
+
+document.body.onload = adicionaContas();
