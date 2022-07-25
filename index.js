c753f0d1e9617d91b01508cc45c8f95edd87c9d4
ricardo <rcavaraujo@gmail.com>
2022-07-24 21:23:06 -0300

    [M1S04] Ex 7 - Conta bancaria Parte 7

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const contasClientes = [
     id: 1,
     nome: 'Cliente 01',
     saldo: 500,
+    senha: 'abc',
     },
     {
     id: 2,
     nome: 'Cliente 02',
     saldo: 3000,
+    senha: 'def',
     },
     {
     id: 3,
     nome: 'Cliente 03',
     saldo: 5000,
+    senha: 'ghi',
     },
     ];
 
@@ -38,13 +41,18 @@ executa.onclick = () => {
     let selecaoConta = optionConta.options[optionConta.selectedIndex].id;
     selecaoConta = parseInt(selecaoConta);
     const conta = contasClientes.find(conta => conta.id === selecaoConta)
-    if(selecaoOperacao.toLowerCase() === "sacar") {
-        console.log('chegou aqui')
-        saca(conta, valor)
-    } else if(selecaoOperacao.toLowerCase() === "depositar") {
-        deposita (conta, valor)
-    }
-    
+    const senha = document.getElementById("senha").value;
+    console.log(senha)
+    if(senha === conta.senha) {
+        if(selecaoOperacao.toLowerCase() === "sacar") {
+            saca(conta, valor)
+        } else if(selecaoOperacao.toLowerCase() === "depositar") {
+            deposita (conta, valor)
+        }
+    } else {
+        const mensagem = document.getElementById("mensagem");
+        mensagem.textContent = "Senha incorreta";
+    }   
 }
 
 // executa.addEventListener('click', saca);
